6f8b00850b56879289dc1a2f04f44ab930aa33cc
Louis <104061901+Lui5ito@users.noreply.github.com>
2025-01-13 14:33:12 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,13 +5,10 @@ if [[ -d "work" ]]
 then
     cd work
 fi
-touch test.py
+
 # On clone les projets suivant
-#git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/universalbands.git
-
-git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/doctorthesis.git
-
 git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/universalbands.git
+git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/doctorthesis.git
 
 #cd universalbands
 #python -m pip install -e .
