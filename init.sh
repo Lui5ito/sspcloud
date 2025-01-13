00b6513b210066a0bfdd49260b8ca9bef3a3dfff
Louis <104061901+Lui5ito@users.noreply.github.com>
2025-01-13 14:26:13 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,13 +7,15 @@ then
 fi
 
 # On clone les projets suivant
-#git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/universalbands.git
+git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/universalbands.git
 
 git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/doctorthesis.git
+
+git clone https://Lui5ito:${GIT_PERSONAL_ACCESS_TOKEN}@github.com/Lui5ito/universalbands.git
 
 #cd universalbands
 #python -m pip install -e .
 
 #cd ..
 #cd doctorthesis/sb_experiments/ub_models/
-chown -R ${USERNAME}:${GROUPNAME} ${HOME}
+#chown -R ${USERNAME}:${GROUPNAME} ${HOME}
